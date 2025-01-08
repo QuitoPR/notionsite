4554e3bebda0e555ce04b5ae7455576ee71e71e2
QuitoPR <quito.pr@hotmail.com>
2025-01-08 23:01:28 +0100

    asdas

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -1,7 +1,7 @@
 import { UserConfig } from "./src/config"
 
 const userConfig: UserConfig = {
-    base_url: "a66dfde0.notionsite-28x.pages.dev",
+    base_url: "notionsite-28x.pages.dev",
     mount: {
         manual: false,
         page_url: 'https://cooing-court-80e.notion.site/Notion-Hugo-175b866d75928031bad4d5732fbd4d6d?pvs=4',
