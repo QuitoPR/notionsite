ac8ed2a9fafd3985eef2d13aff94031a98808926
QuitoPR <quito.pr@hotmail.com>
2025-01-08 22:55:51 +0100

    aa

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "a66dfde0.notionsite-28x.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://cooing-court-80e.notion.site/Alex-Mendez-83c1da7845134746bef4e261a11f3d91',
+        page_url: 'https://cooing-court-80e.notion.site/Notion-Hugo-175b866d75928031bad4d5732fbd4d6d?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
